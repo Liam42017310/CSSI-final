68a8c77189c8e0871c8c680ebc638fc239341fe8
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-03 15:26:04 -0500

    data store

--- buildmymusic/main.py
+++ buildmymusic/main.py
@@ -1,14 +1,35 @@
+#All of the imports we need in order to run data store and parse API info
 
 import webapp2
 import jinja2
 import os
 import json
 from google.appengine.api import urlfetch
-from google.appengine.api import ndb
+from google.appengine.ext import ndb
+
+#loads the jinja environment
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
+
+#initializes the classes that we need to store data
+
+class Like(ndb.Model):
+    title = ndb.StringProperty(required = True)
+    artist = ndb.StringProperty(required = True)
+    album = ndb.StringProperty(required = True)
+
+class User(ndb.Model):
+    email = ndb.StringProperty(required = True)
+    password = ndb.StringProperty(required = True)
+    uname = ndb.StringProperty(required = True)
+    likes = ndb.KeyProperty(Like, repeated = True)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
+
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
