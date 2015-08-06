1ab5afd4fb938e3801f55f1e6cf6074739f3cea6
CSSI 2015 <CSSI@CSSIs-MacBook-Air.local>
2015-08-06 16:50:40 -0500

    commiting

--- buildmymusic/scripts/profile.js
+++ buildmymusic/scripts/profile.js
@@ -1,5 +1,27 @@
-$(function(){
-    $('#clickme').click(function(){
-        $('#uploadme').click();
-    });
+// $( document ).ready(function() {
+    alert( "ready!" );
+
+
+  $(function(){
+      $('#clickme').click(function(){
+          $('#uploadme').click();
+      });
+  });
+
+  function readURL(input) {
+          if (input.files && input.files[0]) {
+              var reader = new FileReader();
+
+              reader.onload = function (e) {
+                  $('#profileimage').attr('src', e.target.result);
+              }
+
+              reader.readAsDataURL(input.files[0]);
+          }
+      }
+
+      $("#imgInp").change(function(){
+          readURL(this);
+      });
+
 });
